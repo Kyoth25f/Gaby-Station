92d8de4f9f5285593fc742ab19930c6334159fd5
Kyoth25f <41803390+Kyoth25f@users.noreply.github.com>
2025-07-08 11:41:28 -0300

    Atualiza o `RobustToolbox` (#232)
    * Update RT to v263.0.0
    
    * Content changes required for physicsmap purge (#37878)
    
    * PhysMap changes
    
    * Fix call
    
    * Avoid tilefriction resolve
    
    yummy
    
    * a
    
    * fix `TryGetContainingContainer` calls
    
    * Add FixedPoint2TypeParser (#38169)
    
    Co-authored-by: Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
    
    * Remove `PhysicsMap` and `MovedGrids`
    
    * chore: Automatically update REUSE headers
    
    * chore: Automatically update REUSE headers
    
    * Remove `PhysicsMap` e `MoverGrid` dos mapa do Gaby
    
    * Bota as newline no final que falta
    
    * chore: Automatically update REUSE headers
    
    * AFEEEEEEEEEEEEEEEE
    
    * chore: Automatically update REUSE headers
    
    * Aplica as reviews
    
    ---------
    
    Co-authored-by: metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
    Co-authored-by: Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
    Co-authored-by: GabyChangelog <agentepanela2@gmail.com>

--- Content.Client/Physics/Controllers/MoverController.cs
+++ Content.Client/Physics/Controllers/MoverController.cs
@@ -16,14 +16,19 @@
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2024 Plykiya <58439124+Plykiya@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 plykiya <plykiya@protonmail.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
+// SPDX-FileCopyrightText: 2025 Kyoth25f <kyoth25f@gmail.com>
+// SPDX-FileCopyrightText: 2025 Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Content.Shared.Alert;
 using Content.Shared.CCVar;
+using Content.Shared.Friction;
 using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Pulling.Components;
 using Content.Shared.Movement.Systems;
