79920d7eeec00ba284c7ae8f633db711c1191814
Kayo Lima <67492897+Kavest0@users.noreply.github.com>
2025-10-11 19:12:13 -0300

    Tweaks de energia e digitais de ipc (#364)
    * adiciona digitais e dreno dinamico de energia pra bateria
    
    * Update SiliconChargeSystem.cs
    
    * Update SiliconChargeSystem.cs
    
    * chore: Automatically update REUSE headers
    
    * chore: Automatically update REUSE headers
    
    ---------
    
    Co-authored-by: GabyChangelog <agentepanela2@gmail.com>

--- Content.Server/_EinsteinEngines/Silicon/Charge/Systems/SiliconChargeSystem.cs
+++ Content.Server/_EinsteinEngines/Silicon/Charge/Systems/SiliconChargeSystem.cs
@@ -1,7 +1,10 @@
 // SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
 // SPDX-FileCopyrightText: 2024 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
+// SPDX-FileCopyrightText: 2025 Kayo Lima <67492897+Kavest0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
@@ -25,8 +28,16 @@ using Content.Server.PowerCell;
 using Robust.Shared.Timing;
 using Robust.Shared.Configuration;
 using Robust.Shared.Utility;
+using Content.Shared.CCVar;
 using Content.Shared.PowerCell.Components;
+using Content.Shared.Mind;
 using Content.Shared.Alert;
+using Content.Server._EinsteinEngines.Silicon.Death;
+using Content.Server._EinsteinEngines.Power.Components;
+// Begin TheDen - IPC Dynamic Power draw
+using Content.Shared.Movement.Components;
+using Robust.Shared.Physics.Components;
+// End TheDen
 
 namespace Content.Server._EinsteinEngines.Silicon.Charge;
 
@@ -41,6 +52,7 @@ public sealed class SiliconChargeSystem : EntitySystem
     [Dependency] private readonly IConfigurationManager _config = default!;
     [Dependency] private readonly PowerCellSystem _powerCell = default!;
     [Dependency] private readonly AlertsSystem _alerts = default!;
+    [Dependency] private readonly SharedJetpackSystem _jetpack = default!; // TheDen - IPC Dynamic Power draw
     public override void Initialize()
     {
         base.Initialize();
@@ -123,7 +135,10 @@ public sealed class SiliconChargeSystem : EntitySystem
             // Maybe use something similar to refreshmovespeedmodifiers, where it's stored in the component.
             // Maybe it doesn't matter, and stuff should just use static drain?
             if (!siliconComp.EntityType.Equals(SiliconType.Npc)) // Don't bother checking heat if it's an NPC. It's a waste of time, and it'd be delayed due to the update time.
+            {
                 drainRateFinalAddi += SiliconHeatEffects(silicon, siliconComp, frameTime) - 1; // This will need to be changed at some point if we allow external batteries, since the heat of the Silicon might not be applicable.
+                drainRateFinalAddi += SiliconMovementEffects(silicon, siliconComp); // TheDen - IPC Dynamic Power draw // Removes between 90% and 0% of the total power draw.
+            }
 
             // Ensures that the drain rate is at least 10% of normal,
             // and would allow at least 4 minutes of life with a max charge, to prevent cheese.
@@ -191,7 +206,7 @@ public sealed class SiliconChargeSystem : EntitySystem
             if (!_random.Prob(Math.Clamp(temperComp.CurrentTemperature / (upperThresh * 5), 0.001f, 0.9f)))
                 return hotTempMulti;
 
-            // Goobstation: Replaced by KillOnOverheatSystem
+            // GoobStation: Replaced by KillOnOverheatSystem
             //_flammable.AdjustFireStacks(silicon, Math.Clamp(siliconComp.FireStackMultiplier, -10, 10), flamComp);
             //_flammable.Ignite(silicon, silicon, flamComp);
             return hotTempMulti;
@@ -202,5 +217,26 @@ public sealed class SiliconChargeSystem : EntitySystem
             return 0.5f + temperComp.CurrentTemperature / thermalComp.NormalBodyTemperature * 0.5f;
 
         return 0;
+    }
+
+    // TheDen - IPC Dynamic Power draw
+    private float SiliconMovementEffects(EntityUid silicon, SiliconComponent siliconComp)
+    {
+        // Calculate dynamic power draw.
+        if (!TryComp(silicon, out MovementSpeedModifierComponent? movement) ||
+            !TryComp(silicon, out PhysicsComponent? physics) ||
+            !TryComp(silicon, out InputMoverComponent? input))
+            return 0;
+
+        if (input.HeldMoveButtons == MoveButtons.None || _jetpack.IsUserFlying(silicon)) // If nothing is being held or jet packing
+        {
+            return siliconComp.DrainPerSecond * siliconComp.IdleDrainReduction * (-1); // Reduces draw by idle drain reduction
+        }
+
+        // LinearVelocity is relative to the parent
+        return Math.Clamp(
+            siliconComp.DrainPerSecond * ((physics.LinearVelocity.Length() / movement.CurrentSprintSpeed) - 1), // Power draw changes as a negative percentage of the movement
+            siliconComp.DrainPerSecond * siliconComp.IdleDrainReduction * (-1), // Should be a maximum of the idle drain reduction (negative)
+            0f); // Minimum reduction is no change to power draw
     }
 }
