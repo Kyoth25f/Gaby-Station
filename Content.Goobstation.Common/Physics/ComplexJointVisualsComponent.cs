87fdfd6d4f09e0245ac1767dac9c08fb84462628
GabyChangelog <agentepanela2@gmail.com>
2025-10-12 13:30:08 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Common/Physics/ComplexJointVisualsComponent.cs
+++ Content.Goobstation.Common/Physics/ComplexJointVisualsComponent.cs
@@ -1,4 +1,6 @@
+// SPDX-FileCopyrightText: 2025 Aviu00 <93730715+Aviu00@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Rouden <149893554+Roudenn@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Roudenn <romabond091@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
