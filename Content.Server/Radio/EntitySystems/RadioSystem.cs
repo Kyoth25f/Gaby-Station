4e1fc8a15f94b083ea6f5b08f65d39227913a5ac
GabyChangelog <agentepanela2@gmail.com>
2025-10-19 12:38:39 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Radio/EntitySystems/RadioSystem.cs
+++ Content.Server/Radio/EntitySystems/RadioSystem.cs
@@ -35,6 +35,7 @@
 // SPDX-FileCopyrightText: 2025 Panela <107573283+AgentePanela@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 cosmosgc <cosmoskitsune@hotmail.com>
+// SPDX-FileCopyrightText: 2025 the biggest bruh <199992874+thebiggestbruh@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
