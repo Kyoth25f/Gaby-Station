0a5b2bc43b6e0d30199ce99e3c7d127e426c3086
GabyChangelog <agentepanela2@gmail.com>
2025-10-25 13:06:54 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Voodoo/VoodooSystem.cs
+++ Content.Goobstation.Server/Voodoo/VoodooSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 JohnJohn <189290423+JohnJJohn@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Shared.Voodoo;
 using Content.Shared.Body.Systems;
 using Content.Shared.Damage;
