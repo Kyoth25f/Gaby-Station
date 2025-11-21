f13f490f6b299d31c931b8db55fe17035fdf0578
fishbait <gnesse@gmail.com>
2025-11-21 18:05:00 +0100

    [Bugfix] whealchair interaction  (#5091)
    init
    
    Co-authored-by: Fishbait <Fishbait@git.ml>

--- Content.Goobstation.Shared/Vehicles/SharedVehicleSystem.cs
+++ Content.Goobstation.Shared/Vehicles/SharedVehicleSystem.cs
@@ -31,8 +31,6 @@ using Content.Shared.Destructible;
 using Content.Goobstation.Maths.FixedPoint;
 using Content.Shared.Damage;
 using Content.Shared.Actions.Components;
-using Content.Shared.Interaction;
-using Content.Shared.Interaction.Components; 
 
 namespace Content.Goobstation.Shared.Vehicles;
 
@@ -46,7 +44,6 @@ public abstract partial class SharedVehicleSystem : EntitySystem
     [Dependency] private readonly SharedBuckleSystem _buckle = default!;
     [Dependency] private readonly SharedMoverController _mover = default!;
     [Dependency] private readonly SharedVirtualItemSystem _virtualItem = default!;
-    [Dependency] private readonly SharedInteractionSystem _interaction = default!;
 
     public static readonly EntProtoId HornActionId = "ActionHorn";
     public static readonly EntProtoId SirenActionId = "ActionSiren";
@@ -242,10 +239,7 @@ public abstract partial class SharedVehicleSystem : EntitySystem
 
     private void Mount(EntityUid driver, EntityUid vehicle)
     {
-
-        var irelay = EnsureComp<InteractionRelayComponent>(driver);
         _mover.SetRelay(driver, vehicle);
-        _interaction.SetRelay(driver, vehicle, irelay);
 
         if (HasComp<TileMovementComponent>(driver))
             EnsureComp<TileMovementComponent>(vehicle);
@@ -259,7 +253,6 @@ public abstract partial class SharedVehicleSystem : EntitySystem
         if (vehicleComp.Driver != driver)
             return;
 
-        RemComp<InteractionRelayComponent>(driver);
         RemComp<RelayInputMoverComponent>(driver);
 
         vehicleComp.Driver = null;
