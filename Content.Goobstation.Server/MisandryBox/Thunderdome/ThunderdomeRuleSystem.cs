0a5bf12acba17fc2136bcc97f0c9bf8ec1f66d24
GabyChangelog <agentepanela2@gmail.com>
2026-03-07 16:12:31 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/MisandryBox/Thunderdome/ThunderdomeRuleSystem.cs
+++ Content.Goobstation.Server/MisandryBox/Thunderdome/ThunderdomeRuleSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Goob Station Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Goobstation.Common.Mind;
 using Content.Goobstation.Server.MisandryBox.Mind;
 using Content.Goobstation.Shared.MisandryBox.Mind;
