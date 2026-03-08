d7bb7f105867ba9486f51f5c539ee4b05b1c4c8d
GabyChangelog <agentepanela2@gmail.com>
2026-03-08 14:13:11 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Discord/DiscordLink/DiscordLink.cs
+++ Content.Server/Discord/DiscordLink/DiscordLink.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Threading.Tasks;
 using Content.Shared.CCVar;
 using NetCord;
 using NetCord.Gateway;
