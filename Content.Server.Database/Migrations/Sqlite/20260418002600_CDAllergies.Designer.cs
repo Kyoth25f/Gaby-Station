f80a9e3bd20b2afa46942dce7f57f052e1dcc5ca
Will-Oliver-Br <164823659+Will-Oliver-Br@users.noreply.github.com>
2026-04-17 21:40:11 -0300

    [BOUNTY] Alergias (#622)
    * Add Allergies (#649)
    
    * Fix allergies migration (#727)
    
    * usando a using correta
    
    * tradução
    
    * correção
    
    * chore: Automatically update REUSE headers
    
    * delete: deleta as migrations
    
    * add: gera migrations
    
    * chore: Automatically update REUSE headers
    
    ---------
    
    Co-authored-by: ash lea <ashkitten@users.noreply.github.com>
    Co-authored-by: dffdff2423 <dffdff2423@gmail.com>
    Co-authored-by: GabyChangelog <agentepanela2@gmail.com>
    Co-authored-by: Kyoth25f <kyoth25f@gmail.com>

--- Content.Server.Database/Migrations/Sqlite/20260418002600_CDAllergies.Designer.cs
+++ Content.Server.Database/Migrations/Sqlite/20260418002600_CDAllergies.Designer.cs
@@ -7,6 +7,7 @@ using System;
 using Content.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -14,9 +15,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Content.Server.Database.Migrations.Sqlite
 {
     [DbContext(typeof(SqliteServerDbContext))]
-    partial class SqliteServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260418002600_CDAllergies")]
+    partial class CDAllergies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
