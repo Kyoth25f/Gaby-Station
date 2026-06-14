59606edc5001833d7d02f44f415358d9bcb3105e
Kyoth25f <kyoth25f@gmail.com>
2026-06-14 09:45:54 -0300

    cria comando para exportar sprites

---
+++ Content.Client/_Dumont/Commands/ExportSpriteCommand.cs
@@ -0,0 +1,32 @@
+using Content.Client.Sprite;
+using JetBrains.Annotations;
+using Robust.Shared.Console;
+
+namespace Content.Client._Dumont.Commands;
+
+[UsedImplicitly]
+public sealed class ExportSpriteCommand : LocalizedCommands
+{
+    [Dependency] private readonly IEntityManager _entityManager = default!;
+
+    public override string Command => "exportsprite";
+
+    public override async void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (args.Length is not 1)
+        {
+            shell.WriteLine(Help);
+            return;
+        }
+
+        if (!NetEntity.TryParse(args[0], out var targetUidNet)
+            || !_entityManager.TryGetEntity(targetUidNet, out var targetUid))
+        {
+            shell.WriteLine("Entidade não encontrada");
+            return;
+        }
+
+        var contentSprite = _entityManager.System<ContentSpriteSystem>();
+        await contentSprite.Export(targetUid.Value);
+    }
+}
